4d0c3ddeef98658bd3da73e038f69e65425fa5bd
alpotapov <al.potapov@hotmail.com>
2015-11-29 14:03:57 +0100

    Bugfix

--- functions_for_yelp_api.py
+++ functions_for_yelp_api.py
@@ -11,7 +11,6 @@ import oauth2
 from data.models import Category, Venue
 
 
-
 API_HOST = 'api.yelp.com'
 DEFAULT_TERM = 'dinner'
 DEFAULT_LOCATION = 'San Francisco, CA'
@@ -68,7 +67,7 @@ def request(host, path, url_params=None):
     return response
 
 
-#def search(term, location):
+# def search(term, location):
 #    """Query the Search API by a search term and location.
 #    Args:
 #        term (str): The search term passed to the API.
@@ -130,100 +129,116 @@ def query_api(term, location):
     business_id = businesses[0]['id']
 
     print u'{0} businesses found, querying business info ' \
-        'for the top result "{1}" ...'.format(
-            len(businesses), business_id)
+          'for the top result "{1}" ...'.format(
+        len(businesses), business_id)
     response = get_business(business_id)
 
     print u'Result for business "{0}" found:'.format(business_id)
     pprint.pprint(response, indent=2)
 
-def get_latitude(jsonitem):        
+
+def get_latitude(jsonitem):
     "Return latitude from json entry"
     return jsonitem['location']['coordinate']['latitude']
-    
-def get_longitude(jsonitem):        
+
+
+def get_longitude(jsonitem):
     "Return latitude from json entry"
-    return jsonitem['location']['coordinate']['longitude']       
+    return jsonitem['location']['coordinate']['longitude']
+
 
 def property_getter(name, jsonitem):
     "Enter name as string"
     return jsonitem[name]
 
+
 def get_name(jsonitem):
-    return property_getter("name",jsonitem)
-    
+    return property_getter("name", jsonitem)
+
+
 def get_price_range(jsonitem):
-    return property_getter('RestaurantsPriceRange2',jsonitem)
-    
+    return property_getter('RestaurantsPriceRange2', jsonitem)
+
+
 def get_id(jsonitem):
-    return property_getter('id',jsonitem)    
-            
-def get_image_url_original(jsonitem)    :        
-    return property_getter('images_url_original',jsonitem)
-    
+    return property_getter('id', jsonitem)
+
+
+def get_image_url_original(jsonitem):
+    return property_getter('images_url_original', jsonitem)
+
+
 def get_rating(jsonitem):
-    return property_getter('rating',jsonitem)
-    
+    return property_getter('rating', jsonitem)
+
+
 def get_country_code(jsonitem):
     "Returns Unicode eg DE"
     return jsonitem['location']['country_code']
-    
+
+
 def get_city(jsonitem):
     return jsonitem['location']['city']
-    
+
+
 def get_postal_code(jsonitem):
     return jsonitem['location']['postal_code']
-    
+
+
 def get_address(jsonitem):
     return jsonitem['location']['address']
-    
-def make_query(term,location,number_of_results):
+
+
+def make_query(term, location, number_of_results):
     "return list of responses of len (number)_of_results"
-    response3 = search2(term,location,number_of_results)
+    response3 = search2(term, location, number_of_results)
     list_of_responses = []
     for i in response3['businesses']:
         list_of_responses.append(i)
-    
+
     return list_of_responses
-    
+
+
 def save_to_database(jsonitem):
-    v = Venue();
-    v.name = getname(jsonitem)
-    v.image_url_original=get_image_url_original(jsonitem)
+    v = Venue()
+    v.name = get_name(jsonitem)
+    v.image_url_original = get_image_url_original(jsonitem)
     v.rating = get_rating(jsonitem)
-    v.price_range=get_price_range(jsonitem)
-    v.yelp_id=get_id(jsonitem)
-    v.country_code=get_country_code(jsonitem)
-    v.city=get_city(jsonitem)
-    v.postal_code=get_postal_code(jsonitem)
-    v.address=get_address(jsonitem)
-    v.lat=get_latitude(jsonitem)
-    v.lon=get_longitude(jsonitem)
-           
+    v.price_range = get_price_range(jsonitem)
+    v.yelp_id = get_id(jsonitem)
+    v.country_code = get_country_code(jsonitem)
+    v.city = get_city(jsonitem)
+    v.postal_code = get_postal_code(jsonitem)
+    v.address = get_address(jsonitem)
+    v.lat = get_latitude(jsonitem)
+    v.lon = get_longitude(jsonitem)
+
     v.save()
     return 0
-    
+
+
 def save_results(list_of_responses1):
     for i in list_of_responses1:
-        save_to_database(i)        
-    
-def group_grade(nWoman,nMan):
-    max_number = max(nWoman,nMan)
-    faktor_man=1
-    faktor_woman=-2
+        save_to_database(i)
+
+
+def group_grade(nWoman, nMan):
+    max_number = max(nWoman, nMan)
+    faktor_man = 1
+    faktor_woman = -2
     sensM = 0.1
-    sensW= 0.2
-    
-    deltaM = 1 if nMan==max_number else ((max_number-nMan)*sensM)
-    deltaW = 1 if nWoman==max_number else ((max_number-nWoman)*sensW)
-    
-    endFactorM = 0 if nMan==0 else (faktor_man*max_number*deltaM)
-    endFactorW = 0 if nWoman==0 else (faktor_woman*max_number*deltaW)
-        
+    sensW = 0.2
+
+    deltaM = 1 if nMan == max_number else ((max_number - nMan) * sensM)
+    deltaW = 1 if nWoman == max_number else ((max_number - nWoman) * sensW)
+
+    endFactorM = 0 if nMan == 0 else (faktor_man * max_number * deltaM)
+    endFactorW = 0 if nWoman == 0 else (faktor_woman * max_number * deltaW)
+
     final_grade = endFactorM + endFactorW
-    
+
     return final_grade
-    
+
 
 def get_notes_from_our_database(jsonitem, category_name):
     # "Input = category type - search our database and return grades"
@@ -236,18 +251,17 @@ def get_notes_from_our_database(jsonitem, category_name):
 
     print categories
 
-
     return 0
-    
-def calculate_category_grade(category_notes,user_category_choice):
+
+
+def calculate_category_grade(category_notes, user_category_choice):
     "Give user_category_choice as list of options eg (""Restaurant"",""Culture"")"
     "Category notes as [6]=culture,[7]=leisure,[8]=shopping,[9]=restaurant"
-    sumi=0  
-    binary_Rest=0
-    binary_Culture=0
-    binary_Leisure=0
-    binary_Shopping=0
-    
+    sumi = 0
+    binary_Rest = 0
+    binary_Culture = 0
+    binary_Leisure = 0
+    binary_Shopping = 0
 
     if "Restaurant" in user_category_choice:
         binary_Rest = 1
@@ -256,55 +270,62 @@ def calculate_category_grade(category_notes,user_category_choice):
     if "Sport" in user_category_choice:
         binary_Leisure = 1
     if "Surprise" in user_category_choice:
-        binary_Rest=0.25
-        binary_Culture=0.25
-        binary_Leisure=0.25
-        binary_Shopping=0.25
-            
+        binary_Rest = 0.25
+        binary_Culture = 0.25
+        binary_Leisure = 0.25
+        binary_Shopping = 0.25
+
     if "Bar" in user_category_choice:
         binary_Rest = 1
     if "Shopping" in user_category_choice:
         binary_Shopping = 1
-        
-    sumi = binary_Rest * category_notes[9] + binary_Culture*category_notes[6] + binary_Leisure*category_notes[7] + binary_Shopping*category_notes[8]
+
+    sumi = binary_Rest * category_notes[9] + binary_Culture * category_notes[6] + binary_Leisure * category_notes[
+        7] + binary_Shopping * category_notes[8]
     return sumi
-        
+
+
 def calculate_manwoman_grade(groupgrade, category_notes, kids_count, goodForKids):
     "goodforKids - parameter from business json['business']['goodForKids']"
     "kids count = how many kids are joining"
-    factorman=0.5
-    factorwoman = 0.5    
-    
-    factorman= 1 if groupgrade>0 else 0
-    factorwoman= 0 if groupgrade>0 else 1
-    sum2 = factorwoman*category_notes[0] + factorman*category_notes[1]
-    
-    if kids_count>0:
+    factorman = 0.5
+    factorwoman = 0.5
+
+    factorman = 1 if groupgrade > 0 else 0
+    factorwoman = 0 if groupgrade > 0 else 1
+    sum2 = factorwoman * category_notes[0] + factorman * category_notes[1]
+
+    if kids_count > 0:
         sum2 *= goodForKids
-    
+
     return sum2
+
 
 def calculate_time_grade(category_notes, user_time_choice):
     "give time user chose (eg Noon) as string"
     "category notes -> morning [3], noon [4], evening [5]"
-    sum3=0
-    
-    binary_Noon=0
-    binary_Evening=0
-    binary_Morning=0
-    if "Noon" in user_category_choice:
-        binary_Noon=1
-    if "Evening" in user_category_choice:
-        binary_Evening=1
-    if "Morning" in user_category_choice:
-        binary_Morning=1
-        
-    sum3 = binary_Morning*category_notes[3] + binary_Noon*category_notes[4] + binary_Evening*category_notes[5]
+    sum3 = 0
+
+    binary_Noon = 0
+    binary_Evening = 0
+    binary_Morning = 0
+    if "Noon" in user_time_choice:
+        binary_Noon = 1
+    if "Evening" in user_time_choice:
+        binary_Evening = 1
+    if "Morning" in user_time_choice:
+        binary_Morning = 1
+
+    sum3 = binary_Morning * category_notes[3] + binary_Noon * category_notes[4] + binary_Evening * category_notes[5]
+
     return sum3
 
 
- def calculate_final_grade(category_notes,nMan,nWoman,kids_count,goodForKids,user_category_choice,user_time_choice):
-     "Returns double"
-     a1 = category_notes
-     groupGrade1 = group_grade(nWoman,nMan)
-     return calculate_category_grade(a1,user_category_choice)*calculate_manwoman_grade(groupGrade1, a1, kids_count, goodForKids)*calculate_time_grade(a1, user_time_choice)
+def calculate_final_grade(category_notes, nMan, nWoman, kids_count, goodForKids, user_category_choice,
+                          user_time_choice):
+    "Returns double"
+    a1 = category_notes
+    groupGrade1 = group_grade(nWoman, nMan)
+    return calculate_category_grade(a1, user_category_choice) * calculate_manwoman_grade(groupGrade1, a1, kids_count,
+                                                                                         goodForKids) * calculate_time_grade(
+        a1, user_time_choice)
